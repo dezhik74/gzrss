8ca2688336b44a64811d8abddd0bfa6e28a5498c
dezhik74 <dezhik@yandex.ru>
2018-12-07 13:55:33 +0300

    ver 0.3 Основа только отбор615. Исполнение flask

--- app/app.py
+++ app/app.py
@@ -3,4 +3,4 @@ from config import Configuration
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
-
+app.secret_key = '5856056'
--- app/view.py
+++ app/view.py
@@ -1,14 +1,21 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash
+from time import sleep
+
+
+def mysleep (sec):
+    pass
+#    sleep (sec)
 
 @app.route('/')
 @app.route('/index')
 def index():
     name_of_master = "Сергей хозяин"
+    flash('You were successfully logged in')
     return render_template('index.html', name = name_of_master)
 
 
 @app.route('/serje')
 def serje():
-    return render_template('serje.html')
+    return render_template('serje.html', foo=mysleep)
 
